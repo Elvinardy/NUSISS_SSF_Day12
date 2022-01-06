fc521e090af3ed5b68322346551d0511400d1cb1
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-01-06 14:01:33 +0800

    second commit

--- src/main/java/nus/iss/workshop/Day/_SpringBoot/Day12SpringBootApplication.java
+++ src/main/java/nus/iss/workshop/Day/_SpringBoot/Day12SpringBootApplication.java
@@ -2,12 +2,21 @@ package nus.iss.workshop.Day._SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Day12SpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day12SpringBootApplication.class, args);
+
+		@Controller
+		@RequestMapping(path = { "/"})
+		public class numGenerator {
+			@GetMapping(produces = {"text.html"})
+		}
 	}
 
 }
